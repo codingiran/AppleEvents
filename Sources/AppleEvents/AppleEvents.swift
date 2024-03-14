227c49981180bdadabf423f797d176cb95c0be2f
CodingIran <CodingIran@gmail.com>
2024-03-14 15:39:10 +0800

    release 0.0.2

--- Sources/AppleEvents/AppleEvents.swift
+++ Sources/AppleEvents/AppleEvents.swift
@@ -7,8 +7,8 @@ import Foundation
 #error("AppleExtension doesn't support Swift versions below 5.5.")
 #endif
 
-/// Current AppleEvents version 1.0.1. Necessary since SPM doesn't use dynamic libraries. Plus this will be more accurate.
-let version = "1.0.1"
+/// Current AppleEvents version 0.0.2. Necessary since SPM doesn't use dynamic libraries. Plus this will be more accurate.
+let version = "0.0.2"
 
 public enum AppleEvent {
     case restart
